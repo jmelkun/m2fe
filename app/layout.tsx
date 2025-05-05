2b1e8e2aafd2a77be4bde5527c6a5858bac94b7d
jmelkun <jared.melkun@gmail.com>
2025-05-05 19:59:19 +0000

    refactor: replace custom ThemeProvider with MantineProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import '@mantine/core/styles.css';
-import { ColorSchemeScript } from '@mantine/core';
+import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { Header } from '@/components/Header/Header';
 import { AuthProvider } from '@/components/Header/AuthProvider';
-import { ThemeProvider } from '@/components/ThemeProvider';
 
 export default function RootLayout({
   children,
@@ -19,12 +18,12 @@ export default function RootLayout({
         />
       </head>
       <body>
-        <ThemeProvider>
+        <MantineProvider>
           <AuthProvider>
             <Header />
             <main>{children}</main>
           </AuthProvider>
-        </ThemeProvider>
+        </MantineProvider>
       </body>
     </html>
   );
